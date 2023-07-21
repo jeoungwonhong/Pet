22ad0577ae2561531b38bc67cfb6e3b2d3988e09
alice <alicehong1819@naver.com>
2023-07-21 21:29:44 +0900

    test 파일 삭제 scmjoin.js수정

--- pet/src/main/webapp/resources/js/scmJoin.js
+++ pet/src/main/webapp/resources/js/scmJoin.js
@@ -2,7 +2,7 @@ $("#userid").blur(function(){
     	var scm_id = $("#userid").val();
     	if(scm_id == "" || scm_id.length < 4){
     		$(".successIdChk").text("이름은 4자 이상 20자 이하로 설정해주세요 :)");
-			$(".successIdChk").css("color", "red");
+			$(".successIdChk").css("color", "#d43915");
 			$("#idDoubleChk").val("false");
     	}else{
     		$.ajax({
@@ -12,11 +12,11 @@ $("#userid").blur(function(){
 				success : function(data) {
 					if (data == 0) {
 						$(".successIdChk").text("사용가능한 아이디입니다.");
-						$(".successIdChk").css("color", "green");
+						$(".successIdChk").css("color", "#2aa000");
 						$("#idDoubleChk").val("true");
 					} else {
 						$(".successIdChk").text("사용중인 아이디입니다 :p");
-						$(".successIdChk").css("color", "red");
+						$(".successIdChk").css("color", "#d43915");
 						$("#idDoubleChk").val("false");
 					}
 				}, error : function() {
